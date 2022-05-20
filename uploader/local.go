90d836b3db9c8d559d2d4ff756f36b8669203b7a
Hizkia Felix Winata <felix@hizkifw.me>
2022-05-21 01:47:46 +0800

    fix(uploader): copy file if rename fails
    Closes #3

--- uploader/local.go
+++ uploader/local.go
@@ -2,6 +2,7 @@ package uploader
 
 import (
 	"context"
+	"io"
 	"net/url"
 	"os"
 	"path/filepath"
@@ -36,9 +37,32 @@ func (l *Local) Upload(ctx context.Context, item *recorder.Recording) (*UploadRe
 		return nil, err
 	}
 
-	// Move the file to the destination
-	if err := os.Rename(item.FilePath, filepath.Join(l.Path, basename)); err != nil {
-		return nil, err
+	// Try to rename the file to the destination
+	err := os.Rename(item.FilePath, filepath.Join(l.Path, basename))
+
+	// If rename fails, try to copy instead.
+	if err != nil {
+		fileIn, err := os.Open(item.FilePath)
+		if err != nil {
+			return nil, err
+		}
+
+		fileOut, err := os.Create(filepath.Join(l.Path, basename))
+		if err != nil {
+			fileIn.Close()
+			return nil, err
+		}
+		defer fileOut.Close()
+
+		_, err = io.Copy(fileOut, fileIn)
+		fileIn.Close()
+		if err != nil {
+			return nil, err
+		}
+
+		if err = os.Remove(item.FilePath); err != nil {
+			return nil, err
+		}
 	}
 
 	publicURL := l.BaseURL
