d3cc7a4f15cfb3c909bd0c6138562fb68a4cd557
1toldyou <86680163+1toldyou@users.noreply.github.com>
2024-04-16 12:06:08 -0700

    run cargo fmt

--- src/module/recorder.rs
+++ src/module/recorder.rs
@@ -482,8 +482,7 @@ impl YTAStatus {
         // 2024/04/16 16:25:31
         let timestamp_re = Regex::new(r"^\d{4}/\d{2}/\d{2} \d{2}:\d{2}:\d{2}")
             .expect("Failed to compile regex for detecting yta output timestamp");
-        let line = if line.len() > 20 && timestamp_re.is_match(line)
-        {
+        let line = if line.len() > 20 && timestamp_re.is_match(line) {
             line[20..].trim()
         } else {
             line
